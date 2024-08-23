54998ae2acea5dbde75df990cfd519cbcf561d98
Samuel Chichester <sam@samuels-mbp.mynetworksettings.com>
2024-08-23 15:40:16 -0400

    Completed Array & String canPlaceFlowers

---
+++ Array & String/canPlaceFlowers.js
@@ -0,0 +1,28 @@
+/* 
+
+You have a long flowerbed in which some of the plots are planted, 
+and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty 
+and 1 means not empty, and an integer n, return true if n new flowers can be 
+planted in the flowerbed without violating the no-adjacent-flowers rule 
+and false otherwise.
+
+*/
+
+var canPlaceFlowers = function(flowerbed, n) {
+  let total_spots = 0;
+  for (i = 0; i < flowerbed.length; i++) {
+    if (
+      flowerbed[i] === 0 &&
+      (i === 0 || flowerbed[i - 1] === 0) &&
+      (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+    ) {
+      total_spots ++;
+      flowerbed[i] = 1;
+      i ++;
+    }
+  }
+
+  return total_spots >= n;
+};
